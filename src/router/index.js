1c4ec5e2f0ba75421e819e7925be92cf1919a4f4
Muhammad Abieb Basnuril <banurilabib@gmail.com>
2024-03-27 21:53:49 +0700

    fixing error

--- src/router/index.js
+++ src/router/index.js
@@ -98,7 +98,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (EditBerita.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/EditBerita.vue'),
+      component: () => import('../views/Editberita.vue'),
       beforeEnter: (to, from, next) => {
         const tokenlogin = VueCookies.get('tokenlogin');
         if (!tokenlogin) {

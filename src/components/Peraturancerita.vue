5d3225faaafcd6f937a2a4f0c9655ae54a99912a
Muhammad Abieb Basnuril <banurilabib@gmail.com>
2024-06-23 19:40:09 +0700

    ✨ feat: Responsive
    Penambahan responsif pada halaman PeraturanCerita

--- src/components/Peraturancerita.vue
+++ src/components/Peraturancerita.vue
@@ -46,8 +46,8 @@ export default {
                 .then(response => {
                     console.log(response)
                     setTimeout(function () {
-                            window.location.reload();
-                        }, 1000);
+                        window.location.reload();
+                    }, 1000);
                     if (response.data.message === "Your admin status is not active, authorization denied!") {
                         toast.error('Status admin masih nonaktif, mohon untuk login kembali jika merasa sudah mengubahnya')
                     }
@@ -63,86 +63,68 @@ export default {
 </script>
 
 <template>
-    <div class="flex bg-offwhite">
+    <div class="flex bg-offwhite min-h-screen">
         <Sidebar />
 
-        <div class="w-[1200px] pl-8 my-4 bg-offwhite">
-            <p class="font=poppins font-bold text-[30px] leading-6 text-sulfurblack mt-8 border-b border-[#D0D5DD] pb-4">
-                Update Aturan Cerita</p>
+        <div class="max-w-[1200px] w-full pl-8 max-md:pl-2 my-4 bg-offwhite flex-grow">
+            <p class="title-style">Update Aturan Cerita</p>
 
             <div v-if="aturanblog.currentPage === 1"
-                class="flex flex-col text-center bg-oceanteal rounded-md mt-16 py-8 px-16 mb-8 m-auto">
-                <div class="font-poppins font-semibold text-[40px] p-8 text-white">
+                class="flex flex-col text-center bg-oceanteal rounded-md mt-8 py-8 px-6 mb-8 mx-auto">
+                <div class="text-[30px] max-lg:text-[25px] max-md:text-[20px] max-sm:text-[17px] text-white font-poppins font-bold">
                     {{ aturanblog.data.title }}
                 </div>
                 <div v-html="aturanblog.data.content"
-                    class="font-poppins font-normal text-[16px] flex flex-col items-center justify-center text-left m-auto w-[673px] text-semitransparentwhite px-8 whitespace-break-spaces">
-
+                    class="font-poppins font-normal text-[14px] md:text-[16px] flex flex-col items-center justify-center text-left mx-auto w-full md:w-[673px] text-semitransparentwhite px-4 md:px-8 whitespace-break-spaces">
                 </div>
-                <div class="flex flex-col justify-end items-end" @click="toggleModalEditAturanBlog()">
-                    <button class="py-1 px-8 bg-white font-poppins font-bold text-[14px] text-teal rounded-lg">Edit</button>
+                <div class="flex justify-end items-end mt-4" @click="toggleModalEditAturanBlog()">
+                    <button
+                        class="py-1 px-6 md:px-8 bg-white font-poppins font-bold text-[14px] text-teal rounded-lg">Edit</button>
                 </div>
-
 
                 <!-- Pop up Modal Aturan Blog -->
-                <div>
-                    <form v-if="showeditaturanblog" @submit.prevent="editaturan()"
-                        class="overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none justify-center flex">
-                        <div class="relative w-auto my-6 mx-auto max-w-6xl">
+                <div v-if="showeditaturanblog">
+                    <form @submit.prevent="editaturan()" class="fixed inset-0 z-50 flex items-center justify-center">
+                        <div class="relative w-full max-w-2xl mx-auto bg-white rounded-lg shadow-lg">
                             <!--content-->
-                            <div
-                                class="border border-red rounded-lg shadow-lg relative flex flex-col max-w-[600px] bg-white outline-none focus:outline-none">
+                            <div class="border rounded-lg shadow-lg flex flex-col bg-white outline-none">
                                 <!--header-->
                                 <div class="flex items-start justify-between p-5 border-b-2 border-black rounded-t">
-                                    <h3 class="text-[40px] text-teal font-semibold font-poppins">
-                                        Edit Aturan Blog
-                                    </h3>
+                                    <h3 class="text-[24px] md:text-[40px] text-teal font-semibold font-poppins">Edit Aturan
+                                        Blog</h3>
                                     <button
-                                        class="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                                        v-on:click="toggleModalEditAturanBlog()">
+                                        class="ml-auto bg-transparent border-0 text-black text-3xl leading-none font-semibold outline-none focus:outline-none"
+                                        @click="toggleModalEditAturanBlog()">
                                         <span
-                                            class="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                                        </span>
+                                            class="bg-transparent text-black h-6 w-6 text-2xl block outline-none focus:outline-none">&times;</span>
                                     </button>
                                 </div>
                                 <!--body-->
-                                <div class="flex flex-col gap-8 relative p-6">
-                                    <div class="flex gap-2 flex-col">
-                                        <label for="Judul" class="font-poppins font-bold text-base text-teal">Aturan Blog
-                                        </label>
+                                <div class="flex flex-col gap-4 md:gap-8 p-6">
+                                    <div class="flex flex-col gap-2">
+                                        <label for="Judul" class="font-poppins font-bold text-base text-teal">Aturan
+                                            Blog</label>
                                         <quill-editor theme="snow" contentType="html"
                                             v-model:content="aturanblog.data.content"></quill-editor>
                                     </div>
                                 </div>
                                 <!--footer-->
-                                <div class="flex items-center justify-center p-6 border-t-2 border-black rounded-b">
-                                    <button
-                                        class="text-white bg-teal border hover:text-white active:bg-teal-600 font-bold uppercase text-sm px-12 py-3 rounded outline-none focus:outline-none mr-1 mb-1   "
-                                        type="submit">
-                                        Simpan
-                                    </button>
-                                    <button
-                                        class="text-teal bg-white border active:bg-teal-600 font-bold uppercase text-sm px-6 py-3 rounded outline-none focus:outline-none mr-1 mb-1"
-                                        type="button" v-on:click="toggleModalEditAturanBlog()">
-                                        batal
-                                    </button>
+                                <div class="flex items-center justify-center p-6 border-t-2 border-black rounded-b gap-4">
+                                    <button type="submit"
+                                        class="text-white bg-teal border hover:text-white active:bg-teal-600 font-bold uppercase text-sm px-8 md:px-12 py-2 md:py-3 rounded outline-none focus:outline-none">Simpan</button>
+                                    <button type="button" @click="toggleModalEditAturanBlog()"
+                                        class="text-teal bg-white border active:bg-teal-600 font-bold uppercase text-sm px-6 md:px-8 py-2 md:py-3 rounded outline-none focus:outline-none">Batal</button>
                                 </div>
                             </div>
-
                         </div>
-
                     </form>
-                    <div v-if="showeditaturanblog" class="opacity-25 fixed inset-0 z-40 bg-black"></div>
+                    <div class="fixed inset-0 z-40 bg-black opacity-25"></div>
                 </div>
-
-            </div>
         </div>
     </div>
-</template>
+</div></template>
 
-<style>
-ol {
+<style>ol {
     list-style-type: decimal;
     margin-left: 1rem;
-}
-</style>
+}</style>

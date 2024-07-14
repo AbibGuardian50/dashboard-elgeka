586879e4bf9bca35e55eaf5b5f0485dc1e73be43
Muhammad Abieb Basnuril <banurilabib@gmail.com>
2024-07-15 01:14:40 +0700

    ✨ feat: New Title
    Adding title into web

--- src/router/index.js
+++ src/router/index.js
@@ -4,158 +4,171 @@ import VueCookies from 'vue-cookies';
 // Fungsi untuk memeriksa autentikasi
 const requireAuth = (to, from, next) => {
   const tokenlogin = VueCookies.get('tokenlogin');
-  if (!tokenlogin) {
+  if (!tokenlogin && to.path !== '/') {
     next('/');
   } else {
     next();
   }
 };
 
+const routes = [
+  {
+    path: '/',
+    name: 'login',
+    component: () => import('../views/Login.vue'),
+    meta: { title: 'Login Admin Elgeka Jawa Barat' }
+  },
+
+  {
+    path: '/about',
+    name: 'about',
+    component: () => import('../views/AboutView.vue'),
+    meta: { title: 'About' }
+  },
+
+  {
+    path: '/donasi',
+    name: 'donasi',
+    component: () => import('../views/Donasi.vue'),
+    meta: { title: 'Donasi' }
+  },
+
+  {
+    path: '/berita',
+    name: 'berita',
+    component: () => import('../views/Informasicml.vue'),
+    meta: { title: 'Informasi CML' }
+  },
+  {
+    path: '/editadmin/:id',
+    name: 'editadmin',
+    component: () => import('../views/Editadmin.vue'),
+    meta: { title: 'Edit Admin Elgeka Jawa Barat' }
+  },
+  {
+    path: '/editberita/:id',
+    name: 'editberita',
+    component: () => import('../views/Editberita.vue'),
+    meta: { title: 'Edit Berita' }
+  },
+  {
+    path: '/editcerita/:id',
+    name: 'editcerita',
+    component: () => import('../views/EditCerita.vue'),
+    meta: { title: 'Edit Cerita' }
+  },
+  {
+    path: '/EditComment/:id',
+    name: 'EditComment',
+    component: () => import('../views/EditComment.vue'),
+    meta: { title: 'Edit Komentar' }
+  },
+  {
+    path: '/editkegiatan/:id',
+    name: 'editkegiatan',
+    component: () => import('../views/Editkegiatan.vue'),
+    meta: { title: 'Edit Kegiatan' }
+  },
+  {
+    path: '/editprofiladmin',
+    name: 'editprofiladmin',
+    component: () => import('../views/EditProfilAdmin.vue'),
+    meta: { title: 'Edit Profil Admin' }
+  },
+  {
+    path: '/editProfilKomunitas',
+    name: 'editProfilKomunitas',
+    component: () => import('../views/EditProfilKomunitas.vue')
+  },
+  {
+    path: '/Editpengurus/:id',
+    name: 'Editpengurus',
+    component: () => import('../views/Editpengurus.vue'),
+    meta: { title: 'Edit Profil Komunitas' }
+  },
+  {
+    path: '/editquotes/:id',
+    name: 'editquotes',
+    component: () => import('../views/Editquotes.vue'),
+    meta: { title: 'Edit Quotes' }
+  },
+  {
+    path: '/kelolaakun',
+    name: 'kelolaakun',
+    component: () => import('../views/Kelolaakun.vue'),
+    meta: { title: 'Kelola Akun Admin Elgeka Jawa Barat' }
+  },
+  {
+    path: '/komunitas',
+    name: 'komunitas',
+    component: () => import('../views/Komunitas.vue'),
+    meta: { title: 'Komunitas' }
+  },
+  {
+    path: '/kegiatan',
+    name: 'kegiatan',
+    component: () => import('../views/Kegiatan.vue'),
+    meta: { title: 'Kegiatan' }
+  },
+  {
+    path: '/notfound',
+    name: 'notfound',
+    component: () => import('../views/Notfound.vue')
+  },
+  {
+    path: '/profilkomunitas',
+    name: 'profilkomunitas',
+    component: () => import('../views/Profilkomunitas.vue'),
+    meta: { title: 'Profil Komunitas Elgeka Jawa Barat' }
+  },
+  {
+    path: '/Penguruskomunitas',
+    name: 'Penguruskomunitas',
+    component: () => import('../views/Penguruskomunitas.vue'),
+    meta: { title: 'Pengurus Komunitas' }
+  },
+  {
+    path: '/peraturancerita',
+    name: 'peraturancerita',
+    component: () => import('../views/Peraturancerita.vue'),
+    meta: { title: 'Peraturan Cerita' }
+  },
+  {
+    path: '/quotes',
+    name: 'quotes',
+    component: () => import('../views/Quotes.vue'),
+    meta: { title: 'Quotes' }
+  },
+  {
+    path: '/UserGuideCerita',
+    name: 'UserGuideCerita',
+    component: () => import('../views/UserGuideEditCerita.vue'),
+    meta: { title: 'User Guide Cerita' }
+  },
+  {
+    path: '/UserGuideQuote',
+    name: 'UserGuideQuote',
+    component: () => import('../views/UserGuideTambahQuote.vue'),
+    meta: { title: 'User Guide Quote' }
+  },
+  {
+    path: '/verifikasicerita',
+    name: 'verifikasicerita',
+    component: () => import('../views/Verifikasicerita.vue'),
+    meta: { title: 'Verifikasi Cerita' }
+  },
+].map(route => ({ ...route, beforeEnter: requireAuth }));
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'login',
-      component: () => import('../views/Login.vue')
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: () => import('../views/AboutView.vue')
-    },
-    {
-      path: '/donasi',
-      name: 'donasi',
-      component: () => import('../views/Donasi.vue'),
-      beforeEnter: requireAuth
-    },
-    {
-      path: '/berita',
-      name: 'berita',
-      component: () => import('../views/Informasicml.vue'),
-      beforeEnter: requireAuth
-    },
-    {
-      path: '/editadmin/:id',
-      name: 'editadmin',
-      component: () => import('../views/Editadmin.vue'),
-      beforeEnter: requireAuth
-    },
-    {
-      path: '/editberita/:id',
-      name: 'editberita',
-      component: () => import('../views/Editberita.vue'),
-      beforeEnter: requireAuth
-    },
-    {
-      path: '/editcerita/:id',
-      name: 'editcerita',
-      component: () => import('../views/EditCerita.vue'),
-      beforeEnter: requireAuth
-    },
-    {
-      path: '/EditComment/:id',
-      name: 'EditComment',
-      component: () => import('../views/EditComment.vue'),
-      beforeEnter: requireAuth
-    },
-    {
-      path: '/editkegiatan/:id',
-      name: 'editkegiatan',
-      component: () => import('../views/Editkegiatan.vue'),
-      beforeEnter: requireAuth
-    },
-    {
-      path: '/editprofiladmin',
-      name: 'editprofiladmin',
-      component: () => import('../views/EditProfilAdmin.vue'),
-      beforeEnter: requireAuth
-    },
-    {
-      path: '/editProfilKomunitas',
-      name: 'editProfilKomunitas',
-      component: () => import('../views/EditProfilKomunitas.vue'),
-      beforeEnter: requireAuth
-    },
-    {
-      path: '/Editpengurus/:id',
-      name: 'Editpengurus',
-      component: () => import('../views/Editpengurus.vue'),
-      beforeEnter: requireAuth
-    },
-    {
-      path: '/editquotes/:id',
-      name: 'editquotes',
-      component: () => import('../views/Editquotes.vue'),
-      beforeEnter: requireAuth
-    },
-    {
-      path: '/kelolaakun',
-      name: 'kelolaakun',
-      component: () => import('../views/Kelolaakun.vue'),
-      beforeEnter: requireAuth
-    },
-    {
-      path: '/komunitas',
-      name: 'komunitas',
-      component: () => import('../views/Komunitas.vue'),
-      beforeEnter: requireAuth
-    },
-    {
-      path: '/kegiatan',
-      name: 'kegiatan',
-      component: () => import('../views/Kegiatan.vue'),
-      beforeEnter: requireAuth
-    },
-    {
-      path: '/notfound',
-      name: 'notfound',
-      component: () => import('../views/Notfound.vue')
-    },
-    {
-      path: '/profilkomunitas',
-      name: 'profilkomunitas',
-      component: () => import('../views/Profilkomunitas.vue'),
-      beforeEnter: requireAuth
-    },
-    {
-      path: '/Penguruskomunitas',
-      name: 'Penguruskomunitas',
-      component: () => import('../views/Penguruskomunitas.vue'),
-      beforeEnter: requireAuth
-    },
-    {
-      path: '/peraturancerita',
-      name: 'peraturancerita',
-      component: () => import('../views/Peraturancerita.vue'),
-      beforeEnter: requireAuth
-    },
-    {
-      path: '/quotes',
-      name: 'quotes',
-      component: () => import('../views/Quotes.vue'),
-      beforeEnter: requireAuth
-    },
-    {
-      path: '/UserGuideCerita',
-      name: 'UserGuideCerita',
-      component: () => import('../views/UserGuideEditCerita.vue'),
-      beforeEnter: requireAuth
-    },
-    {
-      path: '/UserGuideQuote',
-      name: 'UserGuideQuote',
-      component: () => import('../views/UserGuideTambahQuote.vue'),
-      beforeEnter: requireAuth
-    },
-    {
-      path: '/verifikasicerita',
-      name: 'verifikasicerita',
-      component: () => import('../views/Verifikasicerita.vue'),
-      beforeEnter: requireAuth
-    },
-  ]
-})
+  routes
+});
 
-export default router
+router.beforeEach(requireAuth); // Gunakan requireAuth secara global
+
+router.beforeEach((to, from, next) => {
+  document.title = `${to.meta.title} - Nama Aplikasi Anda`;
+  next();
+});
+
+export default router;
